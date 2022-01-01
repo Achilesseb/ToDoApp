a471fa1488ec7fad6ead5744c53a84a088a0aad8
Achilesseb <prisacariuvictor@gmail.com>
2022-01-01 16:34:17 +0200

    added persistetState to local storage so already passed items can be rerendered

--- src/store.js
+++ src/store.js
@@ -1,5 +1,14 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer);
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+const persistedState = localStorage.getItem("reduxState")
+  ? JSON.parse(localStorage.getItem("reduxState"))
+  : {};
+const store = createStore(rootReducer, persistedState, composedEnhancer);
+store.subscribe(() => {
+  localStorage.setItem("reduxState", JSON.stringify(store.getState()));
+});
 export default store;
